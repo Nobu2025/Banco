69cd3af421eb92fdec53991fc5c31dbdeb97aab5
Nobu2025 <20243rd038@utez.edu.mx>
2025-07-04 10:43:53 -0600

    se agregaron mas botones y el consulta

--- src/main/java/com/example/login/Ventana2Controller.java
+++ src/main/java/com/example/login/Ventana2Controller.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -13,31 +14,37 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Ventana2Controller {
-    String Correo = "patata@gmail";
-    String password = "patata";
+public class Ventana2Controller  {
+
     @FXML
     private Label welcomeText;
-
-    public TextField username;
-    public PasswordField pass;
+    @FXML private Button btnConsultar;
+    @FXML private Button btnDepositar;
+    @FXML private Button btnRetirar;
+    @FXML private Button btnSalir;
 
 
 
     @FXML
-    protected void irVentana2(ActionEvent event) throws IOException {
-        String username = this.username.getText();
-        String pass = this.pass.getText();
+    protected void Consulta(ActionEvent event) throws IOException {
 
-        if(Correo.equals(username)&&password.equals(pass)){
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ventana2.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Consultas.fxml"));
         Parent root = fxmlLoader.load();
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         stage.setScene(new Scene(root));
-        stage.setTitle("Ventana 2");
+        stage.setTitle("Consulta");
         stage.show();
         }
 
+    protected void Retirar(ActionEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Consultas.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Consulta");
+        stage.show();
+    }
+
 
     }
-}
