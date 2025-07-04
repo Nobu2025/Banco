25ff6487e6a0a873b641f3e3d6122979d8a845d8
Alexc22_create <20243rd035@utez.edu.mx>
2025-07-04 09:47:10 -0600

    Add C-perfil + E-ligin

---
+++ src/main/java/com/example/login/usuario.java
@@ -0,0 +1,33 @@
+package com.example.login;
+
+public class usuario {
+    private String Correo;
+    private String Password;
+    private String saldo;
+
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+}
+
